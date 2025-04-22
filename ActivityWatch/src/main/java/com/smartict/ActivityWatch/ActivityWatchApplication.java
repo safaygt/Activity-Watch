025e969c9a12db4f41b1585fe626e8929e25e452
safa <safayigit40@gmail.com>
2025-04-22 11:50:20 +0300

    activity watch project started

---
+++ ActivityWatch/src/main/java/com/smartict/ActivityWatch/ActivityWatchApplication.java
@@ -0,0 +1,13 @@
+package com.smartict.ActivityWatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivityWatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivityWatchApplication.class, args);
+	}
+
+}
---
+++ ActivityWatch/src/test/java/com/smartict/ActivityWatch/ActivityWatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartict.ActivityWatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivityWatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
